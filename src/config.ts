eb4c144d8da55fcd49fae1f94c3a6f1d103d6b23
jaclyn <jaclyn@ambrook.com>
2024-03-30 17:23:27 -0400

    fix build??

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export const NEXT_PUBLIC_URL = "https://zizzamia.xyz";
 
-export const NEXT_PUBLIC_URL_BASE = "http://localhost:3000";
+export const NEXT_PUBLIC_URL_BASE =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://poke-degen.vercel.app/";
