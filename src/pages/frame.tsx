482cc93425fce0c82107c9a0059a898066590374
jaclyn <jaclyn@ambrook.com>
2024-03-30 18:09:27 -0400

    frame

--- src/pages/frame.tsx
+++ src/pages/frame.tsx
@@ -1,49 +1,35 @@
-import { getFrameMetadata } from '@coinbase/onchainkit/frame';
-import type { Metadata } from 'next';
+import { FrameMetadata } from '@coinbase/onchainkit/frame';
 import { NEXT_PUBLIC_URL } from '../config';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Poke',
-    },
-    {
-      label: 'Poke back',
-      action: 'post_redirect',
-    },
-    {
-      action: 'link',
-      label: '$POKE leaderboard',
-      target: 'https://www.google.com',
-    },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
-    aspectRatio: '1:1',
-  },
-  input: {
-    text: 'Tell me a boat story',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-});
-
-export const metadata: Metadata = {
-  title: 'straightupjac.xyz',
-  description: 'poke your degen friends',
-  openGraph: {
-    title: 'straightupjac.xyz',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
-
-export default function Page() {
+export default function HomePage() {
   return (
-    <>
-      <h1>straightupjac.xyz</h1>
-    </>
+    <FrameMetadata
+      buttons={[
+        {
+          label: 'Poke',
+        },
+        {
+          label: 'Poke back',
+          action: 'post_redirect',
+        },
+        {
+          action: 'link',
+          label: '$POKE leaderboard',
+          target: 'https://poke-degen.vercel.app/leaderboard',
+        },
+      ]}
+      image={{
+        src: `${NEXT_PUBLIC_URL}/park-3.png`,
+        aspectRatio: '1:1'
+      }}
+      input={{
+        text: 'Tell me a boat story',
+      }}
+      state={{
+        counter: 1,
+      }}
+      postUrl={`${NEXT_PUBLIC_URL}/api/frame`}
+    />
+
   );
 }
