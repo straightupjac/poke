924d3f3b2065d55935a488e248f4eeda3e9e2f27
jaclyn <jaclyn@ambrook.com>
2024-03-30 17:58:48 -0400

    case

--- src/pages/poke.tsx
+++ src/pages/poke.tsx
@@ -1,6 +1,6 @@
 
 import { VStack } from "@chakra-ui/layout";
-import { Poke } from "../components/poke";
+import { Poke } from "../components/Poke";
 import Navigation from "../components/navigation";
 import { FarcasterProfile } from "../components/SignInWithFarcaster";
 
